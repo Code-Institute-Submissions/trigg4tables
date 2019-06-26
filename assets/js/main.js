7f4cb24af09dd7fe813ca3872a0d205238520a8a
Edel O'Sullivan <sullivanedel@hotmail.com>
2019-06-26 15:13:50 +0000

    Reverse press go if else actions.

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,34 +16,26 @@ $(document).ready(function() {
 
 
     //WHAT: Get whats selected by user when they press go. 
-
     $("#selectGo").click(function() {
 
         let first = $("#selectNo").children("option:selected").val();
         let operator = $("#selectOperator").find("input:checked").val();
 
-        if (first > 0 && operator) { console.log(first, operator); }
+        if (!(first>0) || !operator) { alert("Please pick a number and operator. Then press go"); }
 
-        else { alert("Please pick a number and operator. Then press go"); }
-    })
-
-
-
-});
+        else { console.log(first, operator); }
+    });
 
 
+//console.log(first, operator);
 
+//WHAT: Create tables 
 
-
-/*console.log('test');
-
-var first = 9;
-var operator = 'x';
-var myArray = [];
+/*const myArray = [];
 
 for (let i = 1; i < 13; i++) {
     if (operator === '+') {
-        var answer = first + i;
+        let answer = first + i;
         myArray.push({ id: i, key1: first, key2: operator, key3: i, key4: answer });
     }
     else
@@ -67,6 +59,9 @@ console.log(myArray);
 console.log(myArray[2]["key3"]);
 var myArray2 = myArray.slice().sort(function(a, b){return 0.5 - Math.random()}); //random sorting code w3schools https://www.w3schools.com/js/js_array_sort.asp
 console.log(myArray2);*/
+
+});
+
 
 
 // canvas
