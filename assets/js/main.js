49781cbe620210dee013b2a8f575ec61a3d85747
Edel O'Sullivan <sullivanedel@hotmail.com>
2019-06-26 15:38:19 +0000

    Include create table and test arrays in press go js.

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,46 +21,51 @@ $(document).ready(function() {
         let first = $("#selectNo").children("option:selected").val();
         let operator = $("#selectOperator").find("input:checked").val();
 
-        if (!(first>0) || !operator) { alert("Please pick a number and operator. Then press go"); }
+        if (!(first > 0) || !operator) { alert("Please pick a number and operator. Then press go"); }
 
-        else { console.log(first, operator); }
-    });
+        else {
+            console.log(first, operator);
 
 
-//console.log(first, operator);
 
-//WHAT: Create tables 
+            //console.log(first, operator);
 
-/*const myArray = [];
+            //WHAT: Create tables 
 
-for (let i = 1; i < 13; i++) {
-    if (operator === '+') {
-        let answer = first + i;
-        myArray.push({ id: i, key1: first, key2: operator, key3: i, key4: answer });
-    }
-    else
-    if (operator === 'x') {
-        let answer = first * i;
-       myArray.push({ id: i, key1: first, key2: operator, key3: i, key4: answer });
-    }
-    else
-    if (operator === '-') {
-        let j = first + i;
-        myArray.push({ id: i, key1: j, key2: operator, key3: first, key4: i });
-           }
-    else
-    if (operator === '/') {
-        let j = first * i;
-        myArray.push({ id: i, key1: j, key2: operator, key3: first, key4: i });
-    }
-}
+            let tableArray = [];
 
-console.log(myArray);
-console.log(myArray[2]["key3"]);
-var myArray2 = myArray.slice().sort(function(a, b){return 0.5 - Math.random()}); //random sorting code w3schools https://www.w3schools.com/js/js_array_sort.asp
-console.log(myArray2);*/
+            for (let i = 1; i < 13; i++) {
+                if (operator === '+') {
+                    let answer = first + i;
+                    tableArray.push({ id: i, key1: first, key2: operator, key3: i, key4: answer });
+                }
+                else
+                if (operator === 'x') {
+                    let answer = first * i;
+                    tableArray.push({ id: i, key1: first, key2: operator, key3: i, key4: answer });
+                }
+                else
+                if (operator === '-') {
+                    let j = first + i;
+                    tableArray.push({ id: i, key1: j, key2: operator, key3: first, key4: i });
+                }
+                else
+                if (operator === '/') {
+                    let j = first * i;
+                    tableArray.push({ id: i, key1: j, key2: operator, key3: first, key4: i });
+                }
+            }
 
-});
+            console.log(tableArray);
+            let testArray = tableArray.slice().sort(function(a, b) { return 0.5 - Math.random() }); //random sorting code w3schools https://www.w3schools.com/js/js_array_sort.asp
+            console.log(testArray);
+
+
+        } //end of else
+    }); // end of press go
+
+
+}); // end of get document
 
 
 
